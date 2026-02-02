ca939964fd790040fdf49499fba21a75de64c571
kingsleyqiu <kkingsley.qqiu@gmail.com>
2026-02-01 21:14:35 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -8,91 +8,103 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-/* ---------- Parsing ---------- */
-int parse_command(char *line, char **args, char **infile, char **outfile, int *append, int *background) {
-
-    *infile = *outfile = NULL;
+/*This is used so that a command can be broken into small parts that can then be read*/
+int parse_command(char *line, char **args, char **inputf, char **outf, int *append, int *background) {
+    *inputf = *outf = NULL;
     *append = *background = 0;
-
     line[strcspn(line, "\n")] = 0;
-
     int i = 0;
-    char *tok = strtok(line, " ");
-    while (tok) {
-        if (!strcmp(tok, "<")) {
-            *infile = strtok(NULL, " ");
-        } else if (!strcmp(tok, ">")) {
-            *outfile = strtok(NULL, " ");
+    char *x = strtok(line, " ");
+
+    while (x) {
+        if (!strcmp(x, "<")) {
+            *inputf = strtok(NULL, " ");
+        } else if (!strcmp(x, ">")) {
+            *outf = strtok(NULL, " ");
             *append = 0;
-        } else if (!strcmp(tok, ">>")) {
-            *outfile = strtok(NULL, " ");
+        } else if (!strcmp(x, ">>")) {
+            *outf = strtok(NULL, " ");
             *append = 1;
-        } else if (!strcmp(tok, "&")) {
+        } else if (!strcmp(x, "&")) {
             *background = 1;
         } else {
-            args[i++] = tok;
+            args[i++] = x;
         }
-        tok = strtok(NULL, " ");
+        x = strtok(NULL, " ");
     }
     args[i] = NULL;
     return i;
 }
 
-/* ---------- Main ---------- */
+/* main functions */
 int main(int argc, char *argv[]) {
-    FILE *input = stdin;
+    FILE *in = stdin;
     char line[MAX_LINE];
-
-    /* Required by lab */
     setenv("shell", argv[0], 1);
 
+    /* if an error occured */
     if (argc == 2) {
-        input = fopen(argv[1], "r");
-        if (!input) {
+        in = fopen(argv[1], "r");
+        if (!in) {
             perror("batch file");
             exit(1);
         }
     }
 
+    /* if normal then excute this loop */
     while (1) {
-        if (input == stdin)
+        if (in == stdin) {
             print_prompt();
-
-        if (!fgets(line, sizeof(line), input))
+        }
+        if (!fgets(line, sizeof(line), in)) {
             break;
+        }
 
         char *args[MAX_ARGS];
-        char *infile, *outfile;
+        char *inputf, *outf;
         int append, background;
 
-        if (!parse_command(line, args, &infile, &outfile, &append, &background))
+        if (!parse_command(line, args, &inputf, &outf, &append, &background)) {
             continue;
-
-        if (!strcmp(args[0], "quit"))
+        }
+        if (!strcmp(args[0], "quit")) {
             break;
+        }
 
         int saved_in = dup(0), saved_out = dup(1);
 
-        if (infile) {
-            int fd = open(infile, O_RDONLY);
+        if (inputf) {
+            int fd = open(inputf, O_RDONLY);
             dup2(fd, 0);
             close(fd);
         }
-        if (outfile) {
-            int fd = open(outfile,
-                O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC),
-                0644);
+        if (outf) {
+            int fd = open(outf, O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC), 0644);
             dup2(fd, 1);
             close(fd);
         }
 
-        if (!strcmp(args[0], "cd")) cmd_cd(args);
-        else if (!strcmp(args[0], "clr")) cmd_clr();
-        else if (!strcmp(args[0], "dir")) cmd_dir(args);
-        else if (!strcmp(args[0], "environ")) cmd_environ();
-        else if (!strcmp(args[0], "echo")) cmd_echo(args);
-        else if (!strcmp(args[0], "help")) cmd_help();
-        else if (!strcmp(args[0], "pause")) cmd_pause();
+        if (!strcmp(args[0], "cd")) {
+            cmd_cd(args);
+        }
+        else if (!strcmp(args[0], "clr")) {
+            cmd_clr();
+        }
+        else if (!strcmp(args[0], "dir")) {
+            cmd_dir(args);
+        }
+        else if (!strcmp(args[0], "environ")) {
+            cmd_environ();
+        }
+        else if (!strcmp(args[0], "echo")) {
+            cmd_echo(args);
+        }
+        else if (!strcmp(args[0], "help")) {
+            cmd_help();
+        }
+        else if (!strcmp(args[0], "pause")) {
+            cmd_pause();
+        }
         else {
             pid_t pid = fork();
             if (pid == 0) {
@@ -109,13 +121,13 @@ int main(int argc, char *argv[]) {
         dup2(saved_out, 1);
         close(saved_in);
         close(saved_out);
-
         reap_zombies();
     }
 
-    if (input != stdin)
-        fclose(input);
-
+    if (in != stdin) {
+        fclose(in);
+    }
+    
     return 0;
 }
 
